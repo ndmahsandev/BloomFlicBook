c867e9839703b62f900a67d5f8d748395a8bc004
ndmahsandev <ndmahsan.dev@gmail.com>
2026-03-10 14:03:18 +0500

    clean up repo with proper gitignore

--- .next/server/next-font-manifest.js
+++
@@ -1,1 +0,0 @@
-self.__NEXT_FONT_MANIFEST="{\"pages\":{},\"app\":{\"/Applications/XAMPP/xamppfiles/htdocs/p/src/app/layout\":[\"static/media/e4af272ccee01ff0-s.p.woff2\"]},\"appUsingSizeAdjust\":true,\"pagesUsingSizeAdjust\":false}"
